deefba2cf0f64e1f2d31a04ddfd801f88573051b
RinSachymu <torreskarl479@gmail.com>
2026-09-06 02:15:30 +0800

    Wrap App component with HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { HashRouter } from 'react-router-dom' // 👈 Use HashRouter
+import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
 )
