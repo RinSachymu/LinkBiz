9e7a254c55a4867b28eace05173978e9eb27a579
RinSachymu <torreskarl479@gmail.com>
2026-09-06 02:24:53 +0800

    Fix main.tsx: Remove React Router dependency and fix App import

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom' // 👈 Use HashRouter
-import App from './App.jsx'
+import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>,
 )
